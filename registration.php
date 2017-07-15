ff39c7092c2883a3413af1094c1ab5796e7b8a9b
Tran Ngoc Duc <caothu91@gmail.com>
2017-07-15 19:10:14 +0700

    dxvn | fix theme path

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Magento/dxvn',
+    'frontend/Diepxuan/dxvn',
     __DIR__
 );
